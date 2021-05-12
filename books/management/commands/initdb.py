cb44e8468f7e56a2544a16ccacbbff09964e13d6
Melissendra <melissendra77@gmail.com>
2021-05-12 14:32:44 +0200

    insertion category and author table ok, insertion book table bug

--- books/management/commands/initdb.py
+++ books/management/commands/initdb.py
@@ -54,20 +54,69 @@ class Command(BaseCommand):
                 books.append(book)
         return books
 
-    def categories_db(self, books):
+    @staticmethod
+    def categories_db(books):
         """ put books' categories data into the table """
         for book in books:
             categories = book["volumeInfo"].get("categories")
+            if categories is not None:
+                for cat in categories:
+                    try:
+                        Category.objects.bulk_create(
+                            [
+                                Category(category_name=cat)
+                            ]
+                        )
+                    except IntegrityError:
+                        return "No category name available"
 
-            for cat in categories:
-                try:
-                    Category.objects.bulk_create(
-                        [
-                            Category(category_name=cat)
-                        ]
-                    )
-                except IntegrityError:
-                    return "No category name available"
+    @staticmethod
+    def authors_db(books):
+        """ put authors' name into the table """
+        for book in books:
+            authors = book["volumeInfo"].get("authors")
+            if authors is not None:
+                for author in authors:
+                    try:
+                        Author.objects.bulk_create(
+                            [
+                                Author(author_name=author)
+                            ]
+                        )
+                    except IntegrityError:
+                        return "No author name available"
+
+    @staticmethod
+    def books_db(books):
+        """ put all books' data in the table """
+        for data in books:
+            book_info = data["volumeInfo"]
+            book_title = book_info.get("title")
+            book_description = book_info.get("description")
+            book_published = book_info.get("publishedDate")
+            book_image = book_info.get("imageLinks").get("thumbnail")
+            book = Book.objects.create(
+                title=book_title,
+                description=book_description,
+                published_date=book_published,
+                image_url=book_image,
+            )
+
+            categories = []
+            for cat in book_info.get("categories"):
+                category, created = Category.objects.get_or_create(
+                    category_name=cat
+                )
+                categories.append(category)
+                book.categories.add(*categories)
+
+            authors = []
+            for author_name in book_info.get["authors"]:
+                author, created = Author.objects.get_or_create(
+                    author_name=author_name
+                )
+                authors.append(author)
+                book.authors.add(*authors)
 
     def clean_data(self, books):
         return [book for book in books if self.is_valid(book)]
@@ -76,11 +125,14 @@ class Command(BaseCommand):
     def is_valid(book):
         for el in book:
             categories = book["volumeInfo"].get("categories")
-            if categories is None:
+            authors = book["volumeInfo"].get("authors")
+            description = book["volumeInfo"].get("description")
+            image = book["volumeInfo"].get("imageLinks")
+            if (categories and authors and description and image) is None:
                 return False
-            if categories == "":
+            if (categories and authors and description and image) == "":
                 return False
-            if not categories:
+            if not (categories and authors and description and image):
                 return False
 
         return True
@@ -90,4 +142,6 @@ class Command(BaseCommand):
         books_list = self.fetch_books()
         clean_books = self.clean_data(books_list)
         self.categories_db(clean_books)
+        self.authors_db(clean_books)
+        self.books_db(clean_books)
         self.stdout.write(self.style.SUCCESS("Done"))
--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     """ Categories table """
-    category_name = models.CharField(max_length=255, unique=True)
+    category_name = models.CharField(max_length=255, unique=True, null=False, blank=False)
 
     def __str__(self):
         return self.category_name
@@ -12,7 +12,7 @@ class Category(models.Model):
 class Author(models.Model):
     """ Authors table """
 
-    author_name = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255, null=False, blank=False)
 
     def __str__(self):
         return self.author_name
@@ -21,13 +21,13 @@ class Author(models.Model):
 class Book(models.Model):
     """ Books table """
 
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField()
     published_date = models.CharField(max_length=50)
     image_url = models.URLField()
 
     categories = models.ManyToManyField(Category, related_name="books")
-    authors = models.ForeignKey(Author, related_name="books",on_delete=models.CASCADE)
+    authors = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
